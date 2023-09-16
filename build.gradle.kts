5312ffcf51e3d208c6868320cbd64cf7e9131b17
Thanh Tran <thanhtran1324@gmail.com>
2023-09-16 07:22:54 -0400

    initial-commit

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+plugins {
+	java
+	war
+	id("org.springframework.boot") version "3.1.3"
+	id("io.spring.dependency-management") version "1.1.3"
+}
+
+group = "com.thanhdev"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
