cc4c5e1913d50a2b85775a9cb3d60669611ddee9
Thanh Tran <thanhtran1324@gmail.com>
2023-09-25 23:32:41 -0400

    enable CORS

--- src/main/java/com/thanhdev/bb/BbApplication.java
+++ src/main/java/com/thanhdev/bb/BbApplication.java
@@ -2,6 +2,9 @@ package com.thanhdev.bb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class BbApplication {
@@ -10,4 +13,15 @@ public class BbApplication {
 		SpringApplication.run(BbApplication.class, args);
 	}
 
+    //Enable CORS
+     @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/*").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 }
